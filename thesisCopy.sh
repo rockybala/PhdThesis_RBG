22b58e73194f7d3a01e703419d8957f170081012
rocky <rgarg@cern.ch>
2017-07-24 12:12:37 +0530

    1st_version

---
+++ thesisCopy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+password=MukeshRakhi@86
+sshpass -p "${password}" scp -r /home/rocky/Rocky/Work/Study/MyStudy/PhdThesis/PhdThesis_RBG/ rgarg@lxplus.cern.ch:/afs/cern.ch/user/r/rgarg/private/
+####sshpass -p "${password}" scp -r /home/rocky/Rocky/Work/Study/MyStudy/PhdThesis/PhdThesis_RBG/thesis/thesis.tex rgarg@lxplus.cern.ch:/afs/cern.ch/user/r/rgarg/private/
+
+
